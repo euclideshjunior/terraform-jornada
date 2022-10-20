6f37b735538b8ef498180f5d9ec4bb7f4f01249c
Euclides Junior <euclides.junior@ifma.edu.br>
2022-10-20 02:23:52 -0300

    Subindo arquivos para github

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+region       = "nyc1"
+ssh_key_name = "Dellbian"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Passada como Variavel de ambiente
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+variable "region" {
+  default = ""
+}
+
